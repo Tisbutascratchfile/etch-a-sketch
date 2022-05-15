79439340378046a1fd6a8f1b5627642c8a4ec369
tbasf <orwekami@gmail.com>
2022-05-15 20:41:30 +0100

    AddEventListeners to colors and grid size

--- script.js
+++ script.js
@@ -33,10 +33,61 @@ function eraser() {
   let color = `rgb(255, 255, 255)`;
   return color;
 }
+// function tabulaRasa() {
+//   childCells.forEach((element) => {
+//     element.style.backgroundColor = eraser();
+//   });
+// }
+
 function tabulaRasa() {
+  for (let i = 0; i < childCells.length; i++) {
+    childCells[i].style.backgroundColor = "white";
+  }
+}
+
+const rand = document.querySelector(".rand-color");
+const gray = document.querySelector(".gray-color");
+const del = document.querySelector(".del");
+const reset = document.querySelector(".reset");
+const outline = document.querySelector(".outline");
+const gridSize = document.querySelector(".grid-size");
+
+function changeColor(e) {
   childCells.forEach((element) => {
-    element.style.backgroundColor = eraser();
+    if (e.target == rand)
+      element.addEventListener("mouseover", (e) => {
+        element.style.backgroundColor = randRGB();
+      });
+    else if (e.target == gray) {
+      element.addEventListener("mouseover", (e) => {
+        element.style.backgroundColor = grayScale();
+      });
+    } else if (e.target == del) {
+      element.addEventListener("mouseover", (e) => {
+        element.style.backgroundColor = eraser();
+      });
+    } else if (e.target == reset) {
+      grid.addEventListener("click", tabulaRasa);
+    }
   });
 }
 
-genCanvas(16);
+rand.addEventListener("click", changeColor);
+gray.addEventListener("click", changeColor);
+del.addEventListener("click", changeColor);
+reset.addEventListener("click", tabulaRasa);
+
+function gridSizefn() {
+  window.addEventListener("load", () => {
+    dimension = gridSize.value = "16";
+    genCanvas(dimension);
+  });
+
+  gridSize.addEventListener("change", () => {
+    dimension = gridSize.value;
+    tabulaRasa();
+    genCanvas(dimension);
+  });
+}
+
+gridSizefn();
